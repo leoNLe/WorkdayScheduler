abb9c4311ce64e3027d327f849617166e13b88dc
Leo Le <lnle125@gmail.com>
2020-04-01 20:38:09 -0700

    completed load from localStorage if exists. Added functionality to remove plan

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let curDate = new Date();
-let schedule = {};
+let schedule;
 init();
 
 function formatDate(date){
@@ -34,23 +34,37 @@ function getDate() {
 
 function init() {
     $("#currentDay").text(getDate());
+    //Check if there is already schedule in localStorage
     if(localStorage.getItem("schedule")) {
         schedule = JSON.parse(localStorage.getItem("schedule"));
-        // Need to loop through the html and update the text
+        //Grab all textarea in time-block and iterating through the entire array of textArea;
+        $.each( $(".time-block > textarea"),  ( index , element) => {
+            let timeSlot = $(element).attr("time");
+            if(schedule[timeSlot]){
+                $(element).text(schedule[timeSlot]);    
+            }
+        })
+    } else {
+        schedule = {};
     }
-
-    
 }
-
 
 function saveToLocal(){
     localStorage.setItem("schedule", JSON.stringify(schedule));
 }
-function saveInfo() {
-    let textAreaValue = $(this).siblings("textarea").val();
-    let timeSlot = $(this).siblings("p").attr("value");
 
-    schedule[timeSlot] = textAreaValue;
+function saveInfo() {
+
+    let textarea = $(this).siblings("textarea");
+    let textAreaValue = textarea.val();
+    let timeSlot = textarea.attr("time");
+
+    if(textAreaValue === "" && schedule[timeSlot]){
+        //Remove key value pair from schedule if textArea is blank
+        delete schedule[timeSlot];
+    } else if (textAreaValue !== ""){
+        schedule[timeSlot] = textAreaValue;
+    }
     saveToLocal();
 }
 
